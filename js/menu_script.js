f27facc1c7a3add3a719d7ab3215cfad808dc745
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-03-05 07:33:29 -0600

    extra features on menu - scrolling revealing effects for menu groups and menu navbar

--- js/menu_script.js
+++ js/menu_script.js
@@ -23,3 +23,33 @@ document.querySelector("#app").addEventListener("click", function (e) {
   console.log(e);
   console.log(this);
 });
+
+// Reveal menu groups
+const allGroups = document.querySelectorAll(".menu-group");
+const revealGroup = function (entries, observer) {
+  const [entry] = entries;
+  entry.target.classList.remove("menu-group-hidden");
+  observer.unobserve(entry.target);
+};
+const groupObserver = new IntersectionObserver(revealGroup, {
+  root: null,
+  threshold: 0.15,
+});
+allGroups.forEach(function (group) {
+  groupObserver.observe(group);
+  group.classList.add("menu-group-hidden");
+});
+
+// Reveal menu nav bar
+
+const pizzaGroup = document.querySelector("#pizza");
+const revealBar = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  menuNavbar.classList.remove("menu-navbar-hidden");
+};
+const pizzaObserver = new IntersectionObserver(revealBar, {
+  root: null,
+  threshold: 0.9,
+});
+pizzaObserver.observe(pizzaGroup);
