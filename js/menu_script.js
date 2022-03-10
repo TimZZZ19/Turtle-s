7eb9feb2e5ca9a5d767f28e26c9ec9d4328db4a3
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-03-10 17:45:22 -0600

    added scroll progress indicator using Intersection Observer API

--- js/menu_script.js
+++ js/menu_script.js
@@ -1,25 +1,51 @@
 const menuNavbar = document.querySelector(".menu-navbar");
+let activeCategory = menuNavbar.firstElementChild;
+activeCategory.classList.add("menu-category-active");
+
+const updateActiveCategory = (newActiveCategory) => {
+  activeCategory = document.querySelector(".menu-category-active");
+  activeCategory.classList.remove("menu-category-active");
+  newActiveCategory.classList.add("menu-category-active");
+};
 
 menuNavbar.addEventListener("click", (e) => {
+  if (!e.target.matches(".menu-category")) return; //guard clause
+
+  updateActiveCategory(e.target);
+
   e.preventDefault();
-
-  if (e.target.classList.contains("menu-category")) {
-    console.log(e);
-    console.log(this);
-
-    const id = e.target.getAttribute("href");
-    const menuGroupSection = document.querySelector(id);
-    const sectionRect = menuGroupSection.getBoundingClientRect();
-
-    window.scrollTo({
-      left: sectionRect.left + window.scrollX,
-      top: sectionRect.top + window.scrollY - 150,
-      behavior: "smooth",
-    });
-  }
+  const id = e.target.getAttribute("href");
+  const menuGroupSection = document.querySelector(id);
+  const sectionRect = menuGroupSection.getBoundingClientRect();
+  window.scrollTo({
+    left: sectionRect.left + window.scrollX,
+    top: sectionRect.top + window.scrollY - 150,
+    behavior: "smooth",
+  });
 });
 
-document.querySelector("#app").addEventListener("click", function (e) {
-  console.log(e);
-  console.log(this);
+// Show active menu group using Intersection Observer API
+const menuGroups = document.querySelectorAll(".menu-group");
+const options = {
+  root: null,
+  rootMargin: "-49% 0px",
+  threshold: 0,
+};
+
+const groupObserver = new IntersectionObserver((entries, groupObserver) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+
+    if (!entry.isIntersecting) return;
+    const id = entry.target.getAttribute("id");
+    const query = `#menu-category-${id}`;
+    const currentActiveCategory = document.querySelector(query);
+
+    updateActiveCategory(currentActiveCategory);
+    // groupObserver.unobserve(entry.target);
+  });
+}, options);
+
+menuGroups.forEach((menuGroup) => {
+  groupObserver.observe(menuGroup);
 });
