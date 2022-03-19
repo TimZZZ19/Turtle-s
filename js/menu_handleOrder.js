859cf5f714aaf76d418795c76002f5b26d597712
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-03-19 14:49:29 -0500

    added photos for order box

--- js/menu_handleOrder.js
+++ js/menu_handleOrder.js
@@ -13,7 +13,6 @@ OrderBasicForm.activate();
 
 const htmlTag = document.querySelector("html");
 const formContainer = document.querySelector(".formbox");
-
 const menuArea = document.querySelector(".menu");
 
 // Form related variables and elements
@@ -38,7 +37,7 @@ const displayBox = () => {
 };
 
 const closeBox = () => {
-  // first, set common variables back undefined
+  // first, set common variables back to undefined
   currentFoodName = undefined;
   currentFoodDescription = undefined;
   currentQty = undefined;
@@ -60,6 +59,7 @@ const renderForm = (e) => {
   foodInformationElement.classList.add("item__being__selected"); // mark the clicked item as selected
 
   const collectCurrentFoodInfo = () => {
+    // first collect information from the menu item
     const foodNameElement = document.querySelector(
       ".item__being__selected .food-name"
     );
@@ -70,6 +70,7 @@ const renderForm = (e) => {
       ".item__being__selected .food-price"
     );
 
+    // set menu item varialbes
     currentFoodName = foodNameElement.textContent;
     currentFoodDescription = foodDescriptionElement
       ? foodDescriptionElement.textContent
@@ -80,24 +81,46 @@ const renderForm = (e) => {
   };
 
   const renderFoodInfoOnForm = () => {
-    const selectedFoodNameElement = document.querySelector(".order__name");
-    const selectedFoodDescriptionElement = document.querySelector(
+    // selecting elements on the form
+    const foodImgElement = document.querySelector(".order__image");
+    const addImageToForm = (parent, foodName) => {
+      const currentFoodImg = document.createElement("img");
+      const nameWithoutSpace = foodName.split(" ").join("");
+      currentFoodImg.src = `/img/order-imgs/${nameWithoutSpace}.jpg`;
+      currentFoodImg.alt = nameWithoutSpace;
+      if (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+      }
+      parent.appendChild(currentFoodImg);
+    };
+
+    const foodNameElement = document.querySelector(".order__name");
+    const foodDescriptionElement = document.querySelector(
       ".order__description"
     );
     const orderInfoContainer = document.querySelector(".order__info");
 
-    selectedFoodNameElement.textContent = currentFoodName;
+    // adding information to the form
 
-    // some foods don't have description
+    // image
+    addImageToForm(foodImgElement, currentFoodName);
+
+    // name
+    foodNameElement.textContent = currentFoodName;
+
+    // description, some foods don't have description
     if (currentFoodDescription) {
-      selectedFoodDescriptionElement.textContent = currentFoodDescription;
+      foodDescriptionElement.textContent = currentFoodDescription;
       orderInfoContainer.style.height = "13rem";
     } else {
-      selectedFoodDescriptionElement.textContent = "";
+      foodDescriptionElement.textContent = "";
       orderInfoContainer.style.height = "8rem";
     }
 
+    // quantity
     displayedQuantity.textContent = currentQty;
+
+    // price
     displayedPrice.textContent = `$ ${currentFoodPrice}`;
   };
 
